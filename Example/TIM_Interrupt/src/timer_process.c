0d5b4d4c1a4f15ced523156040579f88085ab5b1
yuhao.kuo <st40201@gmail.com>
2017-10-23 21:56:22 +0800

    [modify] change timer interrupt function
    this code is debug code.
    IO toggle -> asm nop.

--- Example/TIM_Interrupt/src/timer_process.c
+++ Example/TIM_Interrupt/src/timer_process.c
@@ -10,11 +10,9 @@
 #include "stm32f4xx_hal.h"
 #include "timer_process.h"
 
-GPIO_PinState state = GPIO_PIN_RESET;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	if(htim == &tim_handle) {
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, state);
-		state = (state == GPIO_PIN_RESET) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+		asm volatile("nop");
 	}
 }
